d783c9ac60c6b7fcb8746985611e185bf6b67f44
MohmedIkram <mohmedikram123@gmail.com>
2021-08-22 16:15:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// const express = require("express");
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/users.js";
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const url = `mongodb+srv://ikram:${process.env.MongoPassword}@cluster0.rlfdm.mongodb.net/BookMyShow`;
+// ||
+// "mongodb://localhost/BookMyShow";
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const con = mongoose.connection;
+con.on("open", () => console.log("MongoDB is connected"));
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (request, respone) => {
+  respone.send("Welcome to node app!!!! Hi Guys");
+});
+
+app.use("/users", userRouter);
+
+app.listen(PORT, () => console.log("The server is started in " + PORT));
